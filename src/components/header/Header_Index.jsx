080e6b0d32a4283e3a3bc868680f1ec6a70dfe53
SrijanKAr <srzan561@gmail.com>
2022-11-02 22:17:05 +0545

    header

--- src/components/header/Header_Index.jsx
+++ src/components/header/Header_Index.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import './Header_Style.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+} from '@fortawesome/free-solid-svg-icons';
 const Header_Index = () => {
   return (
     <div className="header">
       <div className="header_list">
-        <div className="header_list_items"></div>
+        <div className="header_list_items">
+          <FontAwesomeIcon icon={faBed} />
+          <span>Stays</span>
+        </div>
+        <div className="header_list_items">
+          <FontAwesomeIcon icon={faPlane} />
+          <span>Flights</span>
+        </div>
+        <div className="header_list_items">
+          <FontAwesomeIcon icon={faCar} />
+          <span>Car Rentals</span>
+        </div>
+        <div className="header_list_items">
+          <FontAwesomeIcon icon={faBed} />
+          <span>Attractions</span>
+        </div>
+        <div className="header_list_items">
+          <FontAwesomeIcon icon={faTaxi} />
+          <span>Taxi</span>
+        </div>
       </div>
     </div>
   );
